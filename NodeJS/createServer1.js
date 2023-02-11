8b12fab75753b085cb19b98590a26e2f34197445
Rutuja Ainapure <75052049+coderrutuja@users.noreply.github.com>
2023-02-11 20:03:14 +0530

    Delete NodeJS directory

--- NodeJS/createServer1.js
+++
@@ -1,9 +0,0 @@
-var http = require('http'); 
-var cs = require('./createserver.js'); 
-
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("Server 2");
-    
-    res.end(); 
-}).listen(8000); 
